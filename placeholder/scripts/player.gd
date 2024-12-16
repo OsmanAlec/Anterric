dd851adfe85c3a2a6e618c2bdadebd78cf8997ab
Osman <synoosman@gmail.com>
2024-12-16 19:31:38 +0000

    2.5 D initialised

--- placeholder/scripts/player.gd
+++ placeholder/scripts/player.gd
@@ -1,23 +1,32 @@
-extends CharacterBody2D
+extends CharacterBody3D
 
-const RATE = 100
+const RATE = 3
 const SPEED = 4 * RATE
-const ACCELERATION	 = 25 * RATE
+const ACCELERATION = 25 * RATE
 
-func player_movement(input, delta):
+func player_movement(direction, delta):
 
-	if input: 
-		velocity = velocity.move_toward(input * SPEED , delta * ACCELERATION)
+	if direction: 
+		velocity = velocity.move_toward(direction * SPEED, delta * ACCELERATION)
 
 	else: 
-		velocity = velocity.move_toward(Vector2(0,0), delta * ACCELERATION)
+		velocity = velocity.move_toward(Vector3.ZERO, delta * ACCELERATION)
 
 
 func _physics_process(delta):
 
-	var input = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	var direction = Vector3.ZERO
 
-	player_movement(input, delta)
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.z += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.z -= 1
+
+	player_movement(direction, delta)
 
 	move_and_slide()
 		
