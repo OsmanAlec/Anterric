6a5f6e62e129ff40e4608dd327dcc83ba148d5bd
Osman <synoosman@gmail.com>
2025-02-12 15:00:12 +0000

    fixing animations with Harshit and Paul
    Fixed animations and did some logic in player.gd
    
    Co-Authored-By: Artemis Coder <148917320+Artemis-Coder17@users.noreply.github.com>
    Co-Authored-By: Coakly <178673151+Coakly@users.noreply.github.com>

--- placeholder/scripts/player.gd
+++ placeholder/scripts/player.gd
@@ -7,10 +7,12 @@ const DASH_SPEED = 7 * RATE
 var dashing = false
 var canDash = true
 var currentAttack = false
-var state:String = "idle"
+var state:String = "Idle"
+var lastDir: Vector3
+
+@onready var anim_tree = get_node("AnimationTree")
 
 func player_movement(direction, delta):
-	
 	
 	if Input.is_action_just_pressed("dash") and canDash:
 		dashing = true
@@ -19,55 +21,63 @@ func player_movement(direction, delta):
 		$dash_again_timer.start()
 
 	if direction: 
+		lastDir = direction
 		if dashing:
-			velocity = velocity.move_toward(direction * DASH_SPEED, delta * ACCELERATION)
+			state = "Dashing"
+			velocity = velocity.move_toward(direction.normalized() * DASH_SPEED, delta * ACCELERATION)
 		else:
-			velocity = velocity.move_toward(direction * SPEED, delta * ACCELERATION)
-
+			state = "Walking"
+			velocity = velocity.move_toward(direction.normalized() * SPEED, delta * ACCELERATION)
+			if !currentAttack:
+				anim_tree.get("parameters/playback").travel("Walking")
+				anim_tree.set("parameters/Walking/BlendSpace1D/blend_position", direction.x) 
 	else: 
+		state = "Standing"
 		velocity = velocity.move_toward(Vector3.ZERO, delta * ACCELERATION)
-	
+		if !currentAttack:
+			anim_tree.get("parameters/playback").travel("Standing")
+			anim_tree.set("parameters/Standing/BlendSpace1D/blend_position", lastDir.x) 
 
 
 func _physics_process(delta):
 
-	var direction = Vector3.ZERO
-	var facingDir: String = "right"
+	var direction : Vector3
 
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1 
-		$Animations.set_flip_h(true)
 	if Input.is_action_pressed("ui_left"):
 		direction.x -= 1
-		$Animations.set_flip_h(false)
 	if Input.is_action_pressed("ui_down"):
 		direction.z += 1
 	if Input.is_action_pressed("ui_up"):
 		direction.z -= 1
+	
+	direction.normalized()
 
 	player_movement(direction, delta)
-	$Animations.play(state)
 
 	move_and_slide()
 	
-	if !currentAttack:
-		currentAttack = true
-		if Input.is_action_just_pressed("primary_attack"):
-			#Animation.play("primary_attack" + facingDir)
-			pass
-					
+	if Input.is_action_just_pressed("primary_attack"):
+		if !currentAttack and state != "Dashing":
+			currentAttack = true
+			anim_tree.get("parameters/playback").travel(state + " Attack")
+			anim_tree.set("parameters/" + state + " Attack/BlendSpace1D/blend_position", lastDir.x) 
+			$attack_timer.start()
 		
 		
+	###Controls for the camera
 	
+	#add a 'dead-zone' to prevent camera flicking
 	if abs($Camera_Controller.position.x - position.x) > 0.5:
-		$Camera_Controller.position.x = lerp($Camera_Controller.position.x, position.x, 0.1 * RATE)
+		$Camera_Controller.position.x = lerp($Camera_Controller.position.x, position.x, 0.05 * RATE)
+	#Always prevent player from walking off screen
 	if $Camera_Controller.position.z - position.z != 0:
-		$Camera_Controller.position.z = lerp($Camera_Controller.position.z, position.z, 0.1 * RATE)
+		$Camera_Controller.position.z = lerp($Camera_Controller.position.z, position.z, 0.05 * RATE)
 		
 	
 	
-		
-		
+
 
 
 func _on_dash_timer_timeout() -> void:
@@ -75,3 +85,6 @@ func _on_dash_timer_timeout() -> void:
 
 func _on_dash_again_timer_timeout() -> void:
 	canDash = true
+
+func _on_attack_timer_timeout() -> void:
+	currentAttack = false
