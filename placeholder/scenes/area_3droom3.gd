240cc5b27b3700e26a4ef4030f45c9c7b24a2758
Oskar Sadowski <C23341983@mytudublin.ie>
2025-02-16 13:50:12 +0000

    added 5 rooms
    boundary walls still to be made

--- placeholder/scenes/area_3droom3.gd
+++ placeholder/scenes/area_3droom3.gd
@@ -4,13 +4,13 @@ var entered = false
 
 func _on_body_entered(body: PhysicsBody3D) -> void:
 	entered = true
-	print ("in box")
+	
 
 func _on_body_exited(body: PhysicsBody3D) -> void:
 	entered = false 
-	print("notinboc")
+	
 
 func _physics_process(delta):
 	if entered == true:
 
-		print("succwess")
+		get_tree().change_scene_to_file("res://scenes/room_no4.tscn")
