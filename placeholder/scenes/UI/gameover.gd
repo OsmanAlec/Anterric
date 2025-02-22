7ca9cbe85e3c29c00fe35dd6f60722f71e3406e8
Osman <synoosman@gmail.com>
2025-02-22 14:11:18 +0000

    health bar
    - added global data for tracking players stats, for now its just the max health and current health
    - made health.gd load data from playerdata.gd if and only if health is an instance for player node
    - created a hearts.gd for spawning in hearts and display the health bar

--- placeholder/scenes/UI/gameover.gd
+++ placeholder/scenes/UI/gameover.gd
@@ -2,8 +2,9 @@ extends CanvasLayer
 
 
 func _on_retry_pressed() -> void:
+	PlayerData.current_health = PlayerData.max_health
 	get_tree().change_scene_to_file("res://scenes/dungeonrooms/room_no1.tscn")
-
+	
 
 
 func _on_quit_pressed() -> void:
---
+++ placeholder/scripts/UI/hearts.gd
@@ -0,0 +1,39 @@
+extends GridContainer
+
+# Preload heart textures for different health states.
+var heart_full = preload("res://art/UI/heart_1.png")
+var heart_empty = preload("res://art/UI/heart_3.png")
+var heart_half = preload("res://art/UI/heart_2.png") 
+
+func _ready() -> void:
+	# Connect the health_changed signal from the player's Health node.
+	get_parent().get_parent().get_node("Health").health_changed.connect(_on_health_changed)
+
+	# Initialize the UI by setting the max health and updating the hearts display.
+	await update_max_health(PlayerData.max_health)
+	update_health(PlayerData.current_health)
+
+func update_max_health(value):
+	# Create and add TextureRect nodes to represent the player's max health.
+	for i in value / 2:
+		add_child(TextureRect.new()) 
+		var heart = get_child(i) 
+		heart.texture = heart_full  
+		heart.set_expand_mode(5) 
+		heart.custom_minimum_size = Vector2(40, 40)  
+	return
+
+func update_health(value):
+	# Update the heart textures based on the player's current health.
+	for i in get_child_count():
+		if value > i * 2 + 1: 
+			get_child(i).texture = heart_full
+		elif value > i * 2:  
+			get_child(i).texture = heart_half
+		else:  
+			get_child(i).texture = heart_empty
+
+func _on_health_changed(diff: int):
+	# Update PlayerData's current health and refresh the heart display.
+	await PlayerData.change_health(PlayerData.current_health + diff)
+	update_health(PlayerData.current_health)
