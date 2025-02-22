536415b7b6d0dd6a449ea43ef359b870fd6fb583
Oskar Sadowski <C23341983@mytudublin.ie>
2025-02-22 18:50:37 +0000

    room tp thingy

--- placeholder/scripts/dungeonrooms/room_manager.gd
+++ placeholder/scripts/dungeonrooms/room_manager.gd
@@ -1,5 +1,6 @@
 extends Area3D
 
+static var counter = 0
 var entered = false 
 
 func _on_body_entered(body: PhysicsBody3D) -> void:
@@ -11,9 +12,15 @@ func _on_body_exited(body: PhysicsBody3D) -> void:
 func _physics_process(delta):
 	if entered == true:
 		var current_num: int = int(str(get_tree().get_current_scene().get_name())[-1])
-		var room_num: int = randi_range(1,5)
-		print(room_num, current_num)
-		if room_num == current_num:
-			room_num = (room_num + 1) % 5 + 1
-		get_tree().change_scene_to_file("res://scenes/dungeonrooms/room_no" + str(room_num) + ".tscn")
+		var room_num: int = randi_range(1,4)
 		
+		counter = counter + 1 
+		
+		if counter > 5:
+			get_tree().change_scene_to_file("res://scenes/dungeonrooms/room_no5.tscn")
+		else:
+			room_num = (room_num + 1) % 4 + 1
+			get_tree().change_scene_to_file("res://scenes/dungeonrooms/room_no" + str(room_num) + ".tscn")
+		
+		
+		print(Ladybug.get_children_count())
