8024bc5b0595e0cad0bbf97e2645bb9523bdbb45
Osman <synoosman@gmail.com>
2025-02-26 00:45:47 +0000

    Template for worker bee and tick
    -Both enemies now move towards the player
    -Both enemies have a detection radius
    -Both enemies will attack if close enough
    -Both enemies have a time out
    -Trying to get the animations to flip based on last facing direction, much like the player.

---
+++ placeholder/scenes/enemies/workerbee.gd
@@ -0,0 +1,59 @@
+extends CharacterBody3D
+
+@onready var anim: AnimatedSprite3D = $Animations
+
+@export var speed: float = 2.0
+@export var attack_range: float = 0.5
+@export var detection_range: float = 1.7
+
+@onready var player: CharacterBody3D = get_tree().get_first_node_in_group("Player")
+
+var canAttack = true
+var isAttacking = false
+var lastDir: int = -1
+
+func _physics_process(delta):
+	if player and global_position.distance_to(player.global_position) <= detection_range:
+		if not isAttacking:
+			move_towards_player(delta)
+		check_attack()
+	else:
+		if not isAttacking:
+			anim.play("fly")
+
+func move_towards_player(delta):
+	if global_position.distance_to(player.global_position) > attack_range:
+		var direction = (player.global_position - global_position).normalized()
+		velocity = direction * speed
+		lastDir = direction.x
+		print(direction)
+		if not isAttacking:
+			anim.play("fly")
+		if lastDir < 1:
+			anim.flip_h = true
+		else:
+			anim.flip_h = false
+		move_and_slide()
+	else:
+		velocity = Vector3.ZERO
+
+func check_attack():
+	if global_position.distance_to(player.global_position) <= attack_range and canAttack:
+		isAttacking = true
+		anim.play("attack")
+		await get_tree().create_timer(0.3).timeout
+		attack()
+
+func attack():
+	canAttack = false
+	$HitBox/CollisionShape3D.disabled = false
+	await get_tree().create_timer(0.5).timeout
+	$HitBox/CollisionShape3D.disabled = true
+	$attack_cooldown.start()
+	isAttacking = false
+
+func _on_attack_cooldown_timeout() -> void:
+	canAttack = true
+
+func _on_health_health_depleted() -> void:
+	queue_free()
