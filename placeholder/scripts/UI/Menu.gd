f67ed7aac342e615a7f526507ce1565f981ea117
Oskar Sadowski <C23341983@mytudublin.ie>
2025-02-26 15:45:33 +0000

    fixed retry and designed rooms

--- placeholder/scripts/UI/Menu.gd
+++ placeholder/scripts/UI/Menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
+	get_tree().change_scene_to_file("res://scenes/home/ant_hill.tscn")
 
 
 func _on_options_pressed() -> void:
