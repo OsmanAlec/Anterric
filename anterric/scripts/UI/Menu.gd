d260fd1b064b3f04955b454dd91a7f2ba667ddaf
Osman <synoosman@gmail.com>
2025-03-02 17:12:10 +0000

    fixed coin display

--- anterric/scripts/UI/Menu.gd
+++ anterric/scripts/UI/Menu.gd
@@ -1,10 +1,12 @@
 extends Control
 
+func _ready() -> void:
+	GameManager.get_node("Coins").visible = false
+
 
 func _on_play_pressed() -> void:
 	GameManager.get_node("Coins").visible = true
 	get_tree().change_scene_to_file("res://scenes/home/ant_hill.tscn")
-
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
