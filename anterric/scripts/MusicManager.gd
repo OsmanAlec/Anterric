0b5a0b438a8164c1fb101cbe5f0e0cd286d4fd0c
paulcolclough <C23361081@mytudublin>
2025-03-02 17:48:52 +0000

    Music Added
    Added boss music as well as fade in and out for tracks using a tween

--- anterric/scripts/MusicManager.gd
+++ anterric/scripts/MusicManager.gd
@@ -3,6 +3,8 @@ extends Node
 # vars for specific playback
 @export var background_music: AudioStreamPlayer
 @export var anthill_music: AudioStreamPlayer
+@export var boss_music: AudioStreamPlayer
+var tween: Tween
 
 # creates and configures background music player
 func _ready():
@@ -16,13 +18,39 @@ func _ready():
 	anthill_music = AudioStreamPlayer.new()
 	anthill_music.stream = preload("res://music/AntHillMusic(2).wav")
 	add_child(anthill_music)
+	
+	# Create the boss music player
+	boss_music = AudioStreamPlayer.new()
+	boss_music.stream = preload("res://Music/newbossmusic.wav")
+	add_child(boss_music)
+
+
+func fade_out_music(music: AudioStreamPlayer):
+	if music.playing:
+		var tween = create_tween()
+		tween.tween_property(music, "volume_db", -30, 1.0)
+		await tween.finished
+		music.stop()
+
+func fade_in_music(music: AudioStreamPlayer):
+	music.volume_db = -30
+	music.play()
+	var tween = create_tween()
+	tween.tween_property(music, "volume_db", 0, 1.0)
+
 
 func stop_background():
 	if background_music.playing:
-		background_music.stop()
+		fade_out_music(background_music)
 
 func play_anthill_music():
-	anthill_music.play()
+	fade_in_music(anthill_music)
 
 func stop_anthill_music():
-	anthill_music.stop()
+	fade_out_music(anthill_music)
+
+func play_boss_music():
+	fade_in_music(boss_music)
+
+func stop_boss_music():
+	fade_out_music(boss_music)
