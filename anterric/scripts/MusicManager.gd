64e407f448c764be6b664402ff5c2894ab7355d9
paulcolclough <C23361081@mytudublin.ie>
2025-03-04 17:19:02 +0000

    Music Fix
    There wasa  bug with music playback not working, Tween was removed from MusicManager and that seems to have fixed the issue

--- anterric/scripts/MusicManager.gd
+++ anterric/scripts/MusicManager.gd
@@ -4,7 +4,7 @@ extends Node
 @export var background_music: AudioStreamPlayer
 @export var anthill_music: AudioStreamPlayer
 @export var boss_music: AudioStreamPlayer
-var tween: Tween
+#var tween: Tween
 
 # creates and configures the music players
 func _ready():
@@ -27,16 +27,16 @@ func _ready():
 
 func fade_out_music(music: AudioStreamPlayer):
 	if music.playing:
-		var tween = create_tween()
-		tween.tween_property(music, "volume_db", -30, 1.0)
-		await tween.finished
+		#var tween = create_tween()
+		#tween.tween_property(music, "volume_db", -30, 1.0)
+		#await tween.finished
 		music.stop()
 
 func fade_in_music(music: AudioStreamPlayer):
-	music.volume_db = -30
+	#music.volume_db = -30
 	music.play()
-	var tween = create_tween()
-	tween.tween_property(music, "volume_db", 0, 1.0)
+	#var tween = create_tween()
+	#tween.tween_property(music, "volume_db", 0, 1.0)
 
 
 func stop_background():
